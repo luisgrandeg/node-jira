3a16d3ac0b05fa27f6398faab7b289b555a66930
Julien Castelain <jcastelain@gmail.com>
2014-11-06 16:46:05 +0100

    test(requestor): more requestor tests

--- test/requestor.js
+++ test/requestor.js
@@ -1,5 +1,15 @@
 var test = require('tape')
+  , nock = require('nock')
   , Requestor = require('../lib/requestor');
+
+// set up mock
+var mock = nock('http://api.packitos.com')
+  .get('/v1/login')
+  .reply(200, {
+    token: 'xyzASDF12345',
+    username: 'tester',
+    email: 'tester@packlink.com'
+  });
 
 test('Requestor', function (t) {
   t.plan(2);
@@ -11,9 +21,28 @@ test('Requestor', function (t) {
 test('Requestor config', function (t) {
   t.plan(2);
 
-  var r1 = new Requestor({api: 'someUrl'});
-  t.equal(JSON.stringify(r1.config), JSON.stringify({api: 'someUrl'}));
+  var requestor = new Requestor({api: 'someUrl'});
+  t.equal(JSON.stringify(requestor.config), JSON.stringify({api: 'someUrl'}));
 
-  var r2 = new Requestor();
-  t.equal(r2.config, undefined);
+  requestor = new Requestor();
+  t.equal(requestor.config, undefined);
+});
+
+test('Requestor.request', function (t) {
+  t.plan(3);
+
+  var requestor = new Requestor({
+    token: '12345ASDFyxz',
+    api: {
+      uri: 'http://api.packitos.com'
+    }
+  });
+
+  var request = requestor.request('GET', '/v1/login');
+  request.end().then(function (res) {
+    t.equal(res.data.token, 'xyzASDF12345');
+    t.equal(res.data.username, 'tester');
+    t.equal(res.data.email, 'tester@packlink.com');
+  });
+
 });
